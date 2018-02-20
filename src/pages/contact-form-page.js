822ec46ab23438f65e9c6668f254dd4d1e5e9f0f
Dmitry Sky <dmitry.sky@icloud.com>
2018-02-20 12:07:26 -0700

    define new contact and save contact actions

--- src/pages/contact-form-page.js
+++ src/pages/contact-form-page.js
@@ -2,10 +2,14 @@ import React, { Component} from 'react';
 import ContactForm from '../components/contact-form';
 
 class ContactFormPage extends Component {
+  submit = (values) => {
+    console.log(values);
+  }
+
   render() {
     return (
       <div>
-        <ContactForm/>
+        <ContactForm handleSubmit={this.submit} />
       </div>
     )
   }
