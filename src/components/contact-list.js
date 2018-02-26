c016f3bea22a1f30adeb8b83a44dca41d4d6af2b
Dmitry Sky <dmitry.sky@icloud.com>
2018-02-26 15:35:14 -0700

    Pivot phase 1, convert contact list or cards to Table of contact rows

--- src/components/contact-list.js
+++ src/components/contact-list.js
@@ -1,9 +1,62 @@
 import React from 'react';
-import { Card } from 'semantic-ui-react';
+import { Card, Button, Checkbox, Icon, Table } from 'semantic-ui-react';
 import ContactCard from './contact-card';
 import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
 
 export default function ContactList({contacts, deleteContact}){
+
+  const rows = () => {
+    return contacts.map(contact => {
+      return (
+        <Table.Row
+          key={contact._id}>
+          <Table.Cell collapsing>
+            <Checkbox />
+          </Table.Cell>
+          <Table.Cell>{ contact.name.first }</Table.Cell>
+          <Table.Cell>{ contact.name.last }</Table.Cell>
+          <Table.Cell>{ contact.phone }</Table.Cell>
+          <Table.Cell>{ contact.email }</Table.Cell>
+          <Table.Cell><Link to={`/contacts/edit/${contact._id}`} className="ui basic button green">Edit</Link></Table.Cell>
+          <Table.Cell><Button basic color="red" onClick={() => deleteContact(contact._id)} >Delete</Button></Table.Cell>
+        </Table.Row>
+      )
+    })
+  }
+
+  const TableExampleFullWidth = () => (
+    <Table celled compact definition>
+      <Table.Header fullWidth>
+        <Table.Row>
+          <Table.HeaderCell />
+          <Table.HeaderCell>First Name</Table.HeaderCell>
+          <Table.HeaderCell>Last Name</Table.HeaderCell>
+          <Table.HeaderCell>Phone Number</Table.HeaderCell>
+          <Table.HeaderCell>E-mail address</Table.HeaderCell>
+          <Table.HeaderCell>Edit</Table.HeaderCell>
+          <Table.HeaderCell>Delete</Table.HeaderCell>
+        </Table.Row>
+      </Table.Header>
+
+      <Table.Body>
+        {rows()}
+      </Table.Body>
+
+      <Table.Footer fullWidth>
+        <Table.Row>
+          <Table.HeaderCell />
+          <Table.HeaderCell colSpan='6'>
+            <Button floated='right' icon labelPosition='left' primary size='small'>
+              <Icon name='user' /> Add Contact
+            </Button>
+            <Button size='small'>Approve</Button>
+            <Button disabled size='small'>Approve All</Button>
+          </Table.HeaderCell>
+        </Table.Row>
+      </Table.Footer>
+    </Table>
+  )
 
   const cards = () => {
     return contacts.map(contact => {
@@ -17,9 +70,12 @@ export default function ContactList({contacts, deleteContact}){
   }
 
   return (
-    <Card.Group>
-      { cards() }
-    </Card.Group>
+    <div>
+      <Card.Group>
+        { cards() }
+      </Card.Group>
+      { TableExampleFullWidth() }
+    </div>
   )
 }
 
